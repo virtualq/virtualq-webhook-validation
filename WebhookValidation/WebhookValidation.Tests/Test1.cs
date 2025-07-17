4c73a8df88deaaf3079d8d082eafd1dbe097e088
Karol Moroz <karol@moroz.dev>
2025-07-17 15:35:04 +0800

    Generate tests

---
+++ WebhookValidation/WebhookValidation.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace WebhookValidation.Tests;
+
+[TestClass]
+public sealed class WebhookValidationConstructors
+{
+    [TestMethod]
+    public void TestTheTruth()
+    {
+        Assert.IsTrue(true);
+        Assert.IsFalse(false);
+        Assert.AreEqual(2 + 2, 4);
+    }
+}
