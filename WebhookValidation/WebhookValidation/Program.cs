25c0ebffb417d0fb1a1e6d6529d105c7d79ea17b
Karol Moroz <karol@moroz.dev>
2025-07-17 15:22:26 +0800

    Parse and re-encode DER private key

--- WebhookValidation/WebhookValidation/Program.cs
+++ WebhookValidation/WebhookValidation/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.EdEC;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+const string privateKeyDer = "MC4CAQAwBQYDK2VwBCIEIIYGTpWE+vd2n2zN4GV0uNE8wXtVn0GhuHQwWegoZfc1";
+var privKey = (Ed25519PrivateKeyParameters)PrivateKeyFactory.CreateKey(Convert.FromBase64String(privateKeyDer));
+
+var v = new Asn1EncodableVector
+{
+    new DerInteger(0),
+    new AlgorithmIdentifier(EdECObjectIdentifiers.id_Ed25519),
+    new DerOctetString(new DerOctetString(privKey.GetEncoded()))
+};
+
+var pkcs8Seq = new DerSequence(v);
+var actual = Convert.ToBase64String(pkcs8Seq.GetDerEncoded());
+Console.WriteLine(actual == privateKeyDer);
